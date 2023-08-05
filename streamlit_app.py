d9f65c008f5e3a491d5d1caacd011887dc38472a
darkcar2v <138178449+darkcar2v@users.noreply.github.com>
2023-08-05 20:16:29 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,14 +6,8 @@ import streamlit as st
 import datetime
 
 """
-# Welcome to Streamlit!
+# CLASSIC FITNESS CENTER ANALYTICS BOARD
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
-
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
-
-In the meantime, below is an example of what you can do with just a few lines of code:
 """
 
 d = st.date_input("Select the date to display the report", datetime.date(2019, 7, 6))
