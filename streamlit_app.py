4aed3dbce796564a7f4eb7cb64aa55c897124994
darkcar2v <138178449+darkcar2v@users.noreply.github.com>
2023-08-05 20:17:10 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,7 @@ import pandas as pd
 import streamlit as st
 import datetime
 
-""" # CLASSIC FITNESS CENTER ANALYTICS BOARD """
+""" ## CLASSIC FITNESS CENTER ANALYTICS BOARD """
 
 d = st.date_input("Select the date to display the report", datetime.date(2019, 7, 6))
 st.write('Your birthday is:', d)
