dadd79d2d8c1c2c4c03b1cea8fdc22435ace16fe
darkcar2v <138178449+darkcar2v@users.noreply.github.com>
2023-08-05 20:26:44 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,9 +2,9 @@ from collections import namedtuple
 import altair as alt
 import math
 import pandas as pd
-import streamlit as st
 import datetime
 import colored
+import streamlit as st
 
 print(colored(""" ## CLASSIC FITNESS CENTER ANALYTICS BOARD """, 'orange'))
 
