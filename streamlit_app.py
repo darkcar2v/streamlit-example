69038c2ccec1fc63cfd9bba4adc7d7cc6ce5667e
darkcar2v <138178449+darkcar2v@users.noreply.github.com>
2023-08-05 20:32:43 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,9 +3,9 @@ import altair as alt
 import math
 import pandas as pd
 import datetime
-import colored
 import streamlit as st
 
+import colored
 print(colored(""" ## CLASSIC FITNESS CENTER ANALYTICS BOARD """, 'orange'))
 
 d = st.date_input("Select the date to display the report", datetime.date(2019, 7, 6))
